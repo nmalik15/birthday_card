b4574f8fe69dcc46f1e18809bd065464cfbb982e
Nenad Malik <nenmalik@amazon.com>
2024-02-13 23:22:54 +0000

    Added Colection of user information

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-#This is the begginning of this file
+#Intro to the app
+
+print("""
+This is a program that creates a Personalized Birthday Message!
+""")
+
+#Collecting user inforamtion
+
+print('''
+Please enter recepient's name: ''')
+recepient_name = input()
+
+print('''
+Please enter recepient's year of birth: ''')
+recepient_year = int(input())
+
+print('''
+Please enter personalized message for the recepient: ''')
+message = input()
+
+print('''
+Please enter sender's name: ''')
+sender_name = input()
+
